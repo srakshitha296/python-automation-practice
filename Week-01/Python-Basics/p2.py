c6e182851796c95c1d33c9630a2e650a99208c36
srakshitha296 <srakshitha296@gmail.com>
2025-04-06 16:13:41 +0530

    Day 1: Added Python basic practice scripts

---
+++ Week-01/Python-Basics/p2.py
@@ -0,0 +1,5 @@
+st = input("Entera string: ")
+if st[::-1] == st:
+    print("Plaindrome")
+else:
+    print("Not a palindrome")
---
+++ Week-01/Python-Basics/p3.py
@@ -0,0 +1,12 @@
+students = {
+    "Ashwin" :20,
+    "Bhuvan" :60,
+    "Chethan" :30,
+    "Darshan" : 90,
+    "Elvin" :70,
+    "Frojas" :50
+}
+
+for k,v in students.items():
+    if v>70:
+        print(f"{k}: {v}")
